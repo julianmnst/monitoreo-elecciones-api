32216167edd51b5ab939ec566a91aea63aa7f033
Julian <julian.mansueto@silstech.com.ar>
2021-08-19 20:19:15 -0300

    initial commit

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const axios = require('axios')
+const config = require('dotenv').config()
+const { server, app } = require('./server')
+
+const TOKEN = process.env.TWITTER_BEARER_TOKEN
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../', 'client', 'index.html'))
+})
+
+app.get('/tweets', async (req, res) => {
+  console.log(req.query.ht)
+  res.json(await getTweets(`${req.query.ht}`))
+})
+
+
+
+async function getTweets(ht) {
+  const TwitterURL =
+    `https://api.twitter.com/2/tweets/search/recent?query=${ht}`
+    + '&expansions=author_id,attachments.media_keys,referenced_tweets.id,referenced_tweets.id.author_id&tweet.fields=attachments,author_id,public_metrics,created_at,id,in_reply_to_user_id,referenced_tweets,text&user.fields=id,name,profile_image_url,protected,url,username,verified&media.fields=duration_ms,height,media_key,preview_image_url,type,url,width,public_metrics'
+  let res = []
+  try {
+    const response = await axios(
+      TwitterURL,
+      {
+        headers: {
+        Authorization: `Bearer ${TOKEN}`
+        }
+      }
+    );
+
+    const tweets = await response;
+    if (tweets.data) {
+      res = tweets.data
+    }
+  }
+  catch(e) {
+    console.log(e)
+  }
+  return res
+}
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`))
