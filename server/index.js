27c4aa469baa20e61d68741fd51ccc743195a51a
Julian <julian.mansueto@silstech.com.ar>
2021-08-19 20:45:50 -0300

    cors para localhost y verce

--- server/index.js
+++ server/index.js
@@ -7,29 +7,28 @@ const { server, app } = require('./server')
 const TOKEN = process.env.TWITTER_BEARER_TOKEN
 const PORT = process.env.PORT || 3000
 
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://monitor-elecciones.vercel.app/'],
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
+
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../', 'client', 'index.html'))
 })
 
-app.get('/tweets', cors(), async (req, res) => {
-  res.json(await getTweets(`${req.query.ht}`))
+app.get('/tweets', async (req, res) => {
+  res.json(await getTweets('req.query.ht'))
 })
-
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-app.get('/tweets2', cors(corsOptions), async (req, res) => {
-  res.send('42')
-})
-
-
 
 async function getTweets(ht) {
   const TwitterURL =
     `https://api.twitter.com/2/tweets/search/recent?query=${ht}`
-    + '&expansions=author_id,attachments.media_keys,referenced_tweets.id,referenced_tweets.id.author_id&tweet.fields=attachments,author_id,public_metrics,created_at,id,in_reply_to_user_id,referenced_tweets,text&user.fields=id,name,profile_image_url,protected,url,username,verified&media.fields=duration_ms,height,media_key,preview_image_url,type,url,width,public_metrics'
+    + '&expansions=author_id,attachments.media_keys,referenced_tweets.id,referenced_tweets.id.author_id'
+    + '&tweet.fields=attachments,author_id,public_metrics,created_at,id,in_reply_to_user_id,referenced_tweets,text'
+    + '&user.fields=id,name,profile_image_url,protected,url,username,verified'
+    + '&media.fields=duration_ms,height,media_key,preview_image_url,type,url,width,public_metrics'
   let res = []
   try {
     const response = await axios(
