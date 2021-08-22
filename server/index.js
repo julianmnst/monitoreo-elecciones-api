a8a570553c33cd0bd1e68e7cd324a6d1526a4dee
Julian <julian.mansueto@silstech.com.ar>
2021-08-21 21:23:44 -0300

    no more retweets

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const sortTweets = (t) => {
 
 async function getTweets(ht) {
   const TwitterURL =
-    `https://api.twitter.com/2/tweets/search/recent?query=${ht}`
+    `https://api.twitter.com/2/tweets/search/recent?query=${ht} -is:retweet`
     + '&expansions=author_id,attachments.media_keys,referenced_tweets.id,referenced_tweets.id.author_id&tweet.fields=attachments,author_id,public_metrics,created_at,id,in_reply_to_user_id,referenced_tweets,text&user.fields=id,name,profile_image_url,protected,url,username,verified&media.fields=duration_ms,height,media_key,preview_image_url,type,url,width,public_metrics'
   let res = []
   try {
